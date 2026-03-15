d1dc74f9feb83f1d16f214c274860b7764c7fe3b
heavwy <1976545289@qq.com>
2026-03-15 20:26:34 +0800

    增加文件

---
+++ ch03/step_function.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int32)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1) # 指定y轴的范围
+plt.show()
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1) # 指定y轴的范围
+plt.show()
